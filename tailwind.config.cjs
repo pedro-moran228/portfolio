b8faf3514846021222ae39c62dfdc292bc1ed94c
Pedro Moran <pedromoran228@gmail.com>
2023-06-25 16:35:27 -0300

    flex in mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -32,7 +32,7 @@ module.exports = {
         h2: { "@apply text-[40px] sm:text-[43px] font-bold": {} },
         h3: { "@apply text-[20px] sm:text-[24px] font-bold": {} },
         h4: { "@apply text-[18px] sm:text-[20px] font-bold": {} },
-        ".container": { "@apply w-[87%] md:w-[80%] mx-auto max-w-[640px] lg:max-w-[1020px]": {} },
+        ".container": { "@apply w-[87%] md:w-[80%] mx-auto max-w-[640px] lg:max-w-[1080px]": {} },
         ".flex-center": { "@apply flex items-center justify-center": {} },
         ".btn": { "@apply inline-block py-[0.45rem] px-[1rem] text-lg font-medium shadow rounded duration-100": {} },
         ".divider-x": { "@apply container bg-gradient-to-r from-transparent via-[rgba(185,_185,_185,_1)] sm:via-[rgba(185,_185,_185,_0.85)] lg:via-[rgba(185,_185,_185,_0.45)] to-transparent h-[2px]": {} },
