673c8f32bfa8ae00d4765f124ab023e6f6188f05
Pedro Moran <pedromoran228@gmail.com>
2023-09-15 10:20:37 -0300

    new folder system

--- src/components/common/Carousel/Index.tsx
+++ src/components/common/Carousel/Index.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from "preact/hooks";
+import ButtonPrevSlide from "./button-prev-slide/Index";
+import ButtonNextSlide from "./button-next-slide/Index";
 
 export default function Carousel() {
   useEffect(() => {
@@ -159,38 +161,8 @@ export default function Carousel() {
           <img data-slide="2" src="LRS/LRS_2.png" />
           <img data-slide="3" src="LRS/LRS_3.png" />
         </ul>
-        <button
-          id="prev"
-          class="left-0 opacity-0 h-full absolute z-20 top-1/2 -translate-y-1/2 w-[80px] flex-center group-hover:flex hover:scale-125 duration-150 bg-gradient-to-r from-gray-900/50 group-hover:opacity-100"
-        >
-          <svg
-            stroke="#FFF"
-            fill="#FFF"
-            class="rotate-180 transform group-hover:scale-125 transition-transform duration-100"
-            viewBox="0 0 1024 1024"
-            height="20px"
-            width="20px"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M271.653 1023.192c-8.685 0-17.573-3.432-24.238-10.097-13.33-13.33-13.33-35.144 0-48.474L703.67 508.163 254.08 58.573c-13.33-13.331-13.33-35.145 0-48.475 13.33-13.33 35.143-13.33 48.473 0L776.38 483.925c13.33 13.33 13.33 35.143 0 48.473l-480.492 480.694c-6.665 6.665-15.551 10.099-24.236 10.099z"></path>
-          </svg>
-        </button>
-        <button
-          id="next"
-          class="right-0 opacity-0 h-full absolute z-20 top-1/2 -translate-y-1/2 w-[80px] flex-center group-hover:flex hover:scale-125 duration-150 bg-gradient-to-l from-gray-900/50 group-hover:opacity-100"
-        >
-          <svg
-            stroke="#FFF"
-            fill="#FFF"
-            class="transform group-hover:scale-125 transition-transform duration-100"
-            viewBox="0 0 1024 1024"
-            height="20px"
-            width="20px"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M271.653 1023.192c-8.685 0-17.573-3.432-24.238-10.097-13.33-13.33-13.33-35.144 0-48.474L703.67 508.163 254.08 58.573c-13.33-13.331-13.33-35.145 0-48.475 13.33-13.33 35.143-13.33 48.473 0L776.38 483.925c13.33 13.33 13.33 35.143 0 48.473l-480.492 480.694c-6.665 6.665-15.551 10.099-24.236 10.099z"></path>
-          </svg>
-        </button>
+        <ButtonPrevSlide />
+        <ButtonNextSlide />
       </div>
     </section>
   );
---
+++ src/components/common/Carousel/button-next-slide/Index.tsx
@@ -0,0 +1,20 @@
+export default function ButtonNextSlide() {
+  return (
+    <button
+      id="next"
+      class="right-0 opacity-0 h-full absolute z-20 top-1/2 -translate-y-1/2 w-[80px] flex-center group-hover:flex hover:scale-125 duration-150 bg-gradient-to-l from-gray-900/50 group-hover:opacity-100"
+    >
+      <svg
+        stroke="#FFF"
+        fill="#FFF"
+        class="transform group-hover:scale-125 transition-transform duration-100"
+        viewBox="0 0 1024 1024"
+        height="20px"
+        width="20px"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M271.653 1023.192c-8.685 0-17.573-3.432-24.238-10.097-13.33-13.33-13.33-35.144 0-48.474L703.67 508.163 254.08 58.573c-13.33-13.331-13.33-35.145 0-48.475 13.33-13.33 35.143-13.33 48.473 0L776.38 483.925c13.33 13.33 13.33 35.143 0 48.473l-480.492 480.694c-6.665 6.665-15.551 10.099-24.236 10.099z"></path>
+      </svg>
+    </button>
+  );
+}
