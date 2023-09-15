fb9c7fb31f7649775be3182851482d273be33d52
Pedro Moran <pedromoran228@gmail.com>
2023-09-15 10:12:11 -0300

    preact import in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
