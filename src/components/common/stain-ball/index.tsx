b002c0f15f87afefc93397cf223146857558cfa9
Pedro Moran <pedromoran228@gmail.com>
2023-09-23 19:25:26 -0300

    only show the stain on the bottom

--- src/components/common/stain-ball/index.tsx
+++ src/components/common/stain-ball/index.tsx
@@ -1,14 +1,18 @@
 import { useAnimateStainBall } from "./use-animate-stain-ball";
 
+interface props {
+  directionType: "VERTICAL" | "HORIZONTAL" | "CENTER";
+  defaultPosition?: { x: number; y: number };
+  boxId: string;
+  hiddenOnMobile?: boolean;
+}
+
 export const StainBall = ({
   directionType,
   defaultPosition = { x: 50, y: 50 },
   boxId,
-}: {
-  directionType: "VERTICAL" | "HORIZONTAL" | "CENTER";
-  defaultPosition?: { x: number; y: number };
-  boxId: string;
-}) => {
+  hiddenOnMobile,
+}: props) => {
   const { position, stainBallElementRef } = useAnimateStainBall({
     boxId,
     directionType,
@@ -26,12 +30,13 @@ export const StainBall = ({
         transform: "translate(-50%, -50%)",
         width: "25vw",
         height: "25vw",
+        display: hiddenOnMobile ? "none" : "flex",
       }}
     >
       <div class="w-full h-full relative">
-        <i class="opacity-10 xs:animate-[spin_30s_linear_infinite] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-[49%_51%_52%_48%_/_75%_30%_70%_25%]"></i>
-        <i class="opacity-10 xs:animate-[spin_40s_linear_reverse_infinite] bg-gradient-to-r from-purple-500 to-pink-900 rounded-[39%_61%_13%_87%_/_5%_30%_70%_95%]"></i>
-        <i class="opacity-10 xs:animate-[spin_50s_linear_infinite] bg-gradient-to-r from-indigo-500 to-sky-500 rounded-[86%_14%_13%_87%_/_77%_25%_75%_23%]"></i>
+        <i class="opacity-10 animate-[spin_30s_linear_infinite] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-[49%_51%_52%_48%_/_75%_30%_70%_25%]"></i>
+        <i class="opacity-10 animate-[spin_40s_linear_reverse_infinite] bg-gradient-to-r from-purple-500 to-pink-900 rounded-[39%_61%_13%_87%_/_5%_30%_70%_95%]"></i>
+        <i class="opacity-10 animate-[spin_50s_linear_infinite] bg-gradient-to-r from-indigo-500 to-sky-500 rounded-[86%_14%_13%_87%_/_77%_25%_75%_23%]"></i>
       </div>
     </section>
   );
